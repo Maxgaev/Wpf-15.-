3db816b7d92a9433617742c12e1682ac5826a058
Максим Алехин <Max-crf@mail.ru>
2022-02-21 18:13:48 +0700

    Добавьте файлы проекта.

---
+++ WpfЗадание 15. Документы/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+using System.Printing;
+using System.Windows.Markup;
+
+namespace WpfЗадание_15.Документы
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            docV.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(docV.Document, fs);
+            }
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docV.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+    }
+
+}
---
+++ WpfЗадание 15. Документы/XpsDocumentWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Documents;
+
+namespace WpfЗадание_15.Документы
+{
+    internal class XpsDocumentWriter
+    {
+        internal void Write(FixedDocument fixedDocument)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
